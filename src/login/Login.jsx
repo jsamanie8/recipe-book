656c453736ce0a458a89769140a82f20cd69e193
Josh Samaniego <josh.samaniego8@gmail.com>
2021-05-12 07:53:03 -0700

    Added some styling to login page. Need to implement navbar.

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Login = () => {
     const [user, handleUser] = useState({ email: '', password: '' });
@@ -16,19 +18,27 @@ const Login = () => {
 
     return (
         <>
-            <div>
-                <div>Recipe Owner? <Button variant="primary">Click here</Button></div>
-            </div>
-            <Container>
+            <Container className="mt-4">
+                <Row className="justify-content-md-center">
+                    <Col md={{ span: 4, offset: 10 }}>
+                        Recipe Owner? <Button variant="primary">Click here</Button>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="mt-5">
                 <Form>
-                    <Form.Group controlId="formName">
-                        <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" value={user.email} onChange={handleChange}></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="formPassword">
-                        <Form.Label>Password:</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value={user.password} onChange={handleChange}></Form.Control>
-                    </Form.Group>
+                    <Row className="justify-content-md-center">
+                        <Col md={{ span: 4, offset: 0 }}>
+                            <Form.Group controlId="formName">
+                                <Form.Label>Email:</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" value={user.email} onChange={handleChange}></Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="formPassword">
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control type="password" placeholder="Password" value={user.password} onChange={handleChange}></Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Row>
                 </Form>
             </Container>
         </>
